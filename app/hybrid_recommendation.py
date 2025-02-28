966a4a97a0c490332f109eef23c1523fcb8bc526
Kareemunnisa <mailkareemunnisa@gmail.com>
2025-02-28 12:54:52 +0530

    New Updates

--- app/hybrid_recommendation.py
+++ app/hybrid_recommendation.py
@@ -9,6 +9,7 @@ business_df = pd.read_csv("../datasets/business_ideas_meaningful.csv")
 learning_df = pd.read_csv("../datasets/learning_paths_expanded.csv")
 skill_demand_df = pd.read_csv("../datasets/skill_demand_expanded.csv")
 market_trends_df = pd.read_csv("../datasets/market_trends_expanded.csv")
+learning_resources_df = pd.read_csv("../datasets/learning_resources.csv")
 
 # Preprocess data
 def preprocess_text(text):
@@ -16,15 +17,17 @@ def preprocess_text(text):
 
 business_df['Skill_Required'] = business_df['Skill_Required'].apply(preprocess_text)
 learning_df['Skill_Category'] = learning_df['Skill_Category'].apply(preprocess_text)
+learning_resources_df['Skill_Name'] = learning_resources_df['Skill_Name'].apply(preprocess_text)
 
 # Combine all skills for a unified TF-IDF vectorizer
-all_skills = pd.concat([business_df['Skill_Required'], learning_df['Skill_Category']])
+all_skills = pd.concat([business_df['Skill_Required'], learning_df['Skill_Category'], learning_resources_df['Skill_Name']])
 vectorizer = TfidfVectorizer()
 vectorizer.fit(all_skills)
 
 # Transform datasets using the trained vectorizer
 business_tfidf = vectorizer.transform(business_df['Skill_Required'])
 learning_tfidf = vectorizer.transform(learning_df['Skill_Category'])
+learning_resources_tfidf = vectorizer.transform(learning_resources_df['Skill_Name'])
 
 def recommend_business_ideas(user_skills):
     user_vector = vectorizer.transform([user_skills])
@@ -37,6 +40,12 @@ def recommend_learning_paths(user_skills):
     similarity_scores = cosine_similarity(user_vector, learning_tfidf).flatten()
     top_indices = similarity_scores.argsort()[-5:][::-1]
     return learning_df.iloc[top_indices][['Skill_Category', 'Beginner_Level', 'Intermediate_Level', 'Advanced_Level', 'Duration', 'Earning_Potential_Post_Learning']]
+
+def recommend_learning_courses(user_skills):
+    user_vector = vectorizer.transform([user_skills])
+    similarity_scores = cosine_similarity(user_vector, learning_resources_tfidf).flatten()
+    top_indices = similarity_scores.argsort()[-5:][::-1]
+    return learning_resources_df.iloc[top_indices][['Skill_Name', 'Resource_Type', 'Resource_Name', 'Platform', 'Cost', 'Skill_Level', 'Duration', 'Earning_Potential_After_Learning']]
 
 def suggest_trends_and_tips():
     return market_trends_df.sample(3)[['Industry', 'Current_Trend', 'Future_Potential']]
@@ -52,7 +61,9 @@ def hybrid_recommendation():
         print("\nBusiness Ideas:\n", business_recs.to_string(), "\n\nTrends:\n", trends.to_string())
     else:
         learning_recs = recommend_learning_paths(user_skills)
+        courses_recs = recommend_learning_courses(user_skills)
         print("\nRecommended Learning Paths:\n", learning_recs.to_string())
+        print("\nRecommended Learning Resources:\n", courses_recs.to_string())
 
 # Run the interactive recommendation system
 if __name__ == "__main__":
